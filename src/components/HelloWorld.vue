5fdc1d569ca05937d6de365a8dd79d6abe73bde4
xjwangchuan <vuexjwangchuan@163.com>
2019-06-14 13:50:08 +0800

    vant,stylus的配置添加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,21 +6,29 @@
     <h1>{{ msg }}</h1>
     <h1>{{ msg }}</h1>
     <h1>{{ msg }}</h1>
-    <h1>哈哈哈哈</h1>
+    <h1 class="my">哈哈哈哈</h1>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
+import { Button } from "vant";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    [Button.name]: Button
+  },
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
 h3 {
   margin: 40px 0 0;
 }
@@ -35,4 +43,8 @@ li {
 a {
   color: #42b983;
 }
+.hello 
+  border 1px solid red
+  .my 
+    background-color black  
 </style>
